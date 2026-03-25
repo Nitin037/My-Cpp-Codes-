e425ee9a595f14ff81cb7e2bec69e922c037236d
Nitin Balwada <nitinbalwada07@gmail.com>
2026-03-25 21:24:27 +0530

    first commit

---
+++ Code.cpp
@@ -0,0 +1,144 @@
+// #include<iostream>
+// using namespace std; 
+// int main() { 
+//     cout << "Hello world" ;
+//     return 0;
+// }
+
+// #include<iostream>
+// #define PI 3.14 
+// using namespace std; 
+// int main() { 
+//     cout << PI << endl;
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std; 
+// int main() { 
+//     cout << "****\n***\n**\n*\n" ;
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std; 
+// int main() { 
+//     int a = 5 ;
+
+//     cout << a ;
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std; 
+// int main() {
+//     int age =25; 
+//     int marks = 200;
+//     cout<<"size of int= "<<sizeof(int)<<endl;
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int age = 25;
+//     int marks = 200;
+//     cout<<"size of int = "<<sizeof(int)<<endl;
+//     return 0;
+
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     char grade = 'A';
+//     cout<<"sizeof char =" <<sizeof(char)<<endl;
+//     return 0; 
+
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int age = 25;
+//     int marks = 200;
+//     char grade = 'A';   
+//     bool isAdult = true;
+//     cout<<age << ""<< marks <<""<<grade<<""<<isAdult<<endl; 
+//     cout<<"size of bool = "<<sizeof(bool)<<endl;
+//     return 0;
+    
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int age ; 
+//     cout<< "Enter your age : ";
+//     cin>> age ;
+//     cout<< "Your age is : " << age << endl;
+//     return 0;
+
+// }  
+
+// #include<iostream>
+// using namespace std ;
+// int main() {
+//     int side ;
+//     cout<<"side is ";
+//     cin>>side;
+//     cout<<"area is "<<side*side<<endl;
+//     return 0 ; 
+// }
+
+// #include<iostream>
+// using namespace std ;
+// int main() {
+//     float pencilcost;
+//     float pencost;
+//     float erasercost;
+//     float Totcost = pencilcost + pencost + erasercost ;
+//     cout<<"cost of pencil =";
+//     cin>>pencilcost;
+
+//     cout<<"cost of pen =";
+//     cin>>pencost;
+
+
+//     cout<<"cost of eraser =";
+//     cin>>erasercost;
+    
+//     cout<<"Total Cost"<<(Totcost + (0.18 + Totcost)) <<endl;
+//     return 0;
+
+
+// }
+
+// #include<iostream>
+// using namespace std ; 
+// int main() {
+//     int pricipal ;
+//     int rate ;
+//     int time ;
+//     cin>>pricipal;
+//     cin>>rate;
+//     cin>>time;
+
+//     cout<<"simple interest : "<<(pricipal*rate*time)/100<<endl;
+//     return 0 ;
+
+
+// }
+
+#include<iostream>
+using namespace std ;
+int main() {
+    int r ;
+    float Area ; 
+    cout<<"radius is : " ;
+    cin>>r;
+    Area = 3.14*r*r ;
+    cout<<"Area is : "<<Area<<endl;
+    return 0 ;
+
+}
+
