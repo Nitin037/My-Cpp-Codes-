e8d64fcb54e4069a970b2d5af53ac5f177757c8c
Nitin Balwada <nitinbalwada4@gmail.com>
2026-06-02 21:39:54 +0530

    update

--- Revising.cpp
+++ Revising.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 using namespace std;
 
-int main()
-{
-    cout<<"Hello "<<endl;
-    cout<<"sizeof"<<endl;
-    return 0 ; 
+// int main()
+// {
+//     cout<<"Hello "<<endl;
+//     cout<<"sizeof"<<endl;
+//     return 0 ; 
+// }
+
+
+int main(){
+    int *ptr = NULL;
+    cout<<ptr<<"\n";
+    cout<<*ptr<<"\n";
+    cout<<"bye";
+
+    return 0 ;
 }
